33264ddd6316b2da0320c35e43cde25ac52e70d5
Domi <d.flierl@oth-aw.de>
2018-11-28 13:04:03 +0100

    Backend: added fontawesome, create header

---
+++ backend/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <!-- Head of main page -->
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Wimowe CMS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        <div id="header">
+            <h1>CMS Konfigurator 2.1</h1>
+            <i class="fas fa-user"></i>
+
+        </div>
+        <div class="wrapper">
+        
+        </div>
+    </body>
+</html>
