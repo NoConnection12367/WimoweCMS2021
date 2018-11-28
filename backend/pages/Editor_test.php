b439dfe0dce246975ad06aa215a461fbc6cf1aec
GreenHeinz <ark_xell@hotmail.com>
2018-11-28 13:05:26 +0100

    Added Rich text Editor

---
+++ backend/pages/Editor_test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <!-- Head of main page -->
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Wimowe CMS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/js/medium-editor.min.js"></script>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css" type="text/css" media="screen" charset="utf-8">
+
+        <script src="../../js/jquery-3.3.1.min.js"></script>
+        <script src="../js/editor_script.js"></script>
+    </head>
+
+    <body>
+    <div id="editor_content" class="editable"></div>
+    <button id="savebutton" type="button">Save!</button> 
+    </body>
+</html>
