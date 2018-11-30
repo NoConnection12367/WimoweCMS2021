dad924c525544c37fead3b5fcec521f6c78a7a72
GreenHeinz <ark_xell@hotmail.com>
2018-11-30 16:08:58 +0100

    Editor kann Content in die Db speichern.

--- backend/js/editor_script.js
+++ backend/js/editor_script.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function(){
     var editor = new MediumEditor('.editable');
 
-    $( "#savebutton" ).click(function() {
-        console.log($('#editor_content').html());
-      });
-});
+    $("#savebutton").click(function(){
+        $.post( "../pages/content_saver.php", 
+        { content: $('#editor_content').html(),
+          siteid: $("#select_option option:selected").val(),
+          tag: "text"
+    },
+        function(status){
+            alert("Status: " + status);
+        });
+    });
 
+    
+});
--- backend/pages/Editor_test.php
+++ backend/pages/Editor_test.php
@@ -13,9 +13,67 @@
         <script src="../../js/jquery-3.3.1.min.js"></script>
         <script src="../js/editor_script.js"></script>
     </head>
+    <?php
+    // Connect to sql database
 
+    // Define constants
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    } else {
+        mysqli_set_charset($conn, "utf8");
+        mysqli_select_db($conn, "wimowe");
+    }
+    ?>
     <body>
-    <div id="editor_content" class="editable"></div>
-    <button id="savebutton" type="button">Save!</button> 
+
+        <!-- Seitenauswahl -->
+        <div>
+        <select id="select_option">
+            <?php
+            $sql = "SELECT * FROM site";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                $sites = array();
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($sites, [$row["ID"], $row["Name"]]);
+                    echo "<option value=\"" . $row["ID"] . "\">" . $row["Name"] . "</option>\n";
+                }
+            }
+            ?>
+        </select>
+        </div>
+
+        <!-- Contentauswahl -->
+        <div>
+        <select id="get_content">
+        <option value ="new_content">Neuer Eintrag</option>
+            <?php
+            $sql = "SELECT * FROM content_de";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                $sites = array();
+                while ($row = mysqli_fetch_assoc($result)) {
+                    array_push($sites, [$row["ID"], $row["Content"]]);
+                    echo "<option value=\"" . $row["ID"] . "\">" . $row["Content"] . "</option>\n";
+                }
+            }
+            ?>
+        </select>
+        </div>
+
+        <br>
+        <div id="editor_content" class="editable"></div>
+        <br>
+        <button id="savebutton" type="button">Save!</button>
     </body>
 </html>
