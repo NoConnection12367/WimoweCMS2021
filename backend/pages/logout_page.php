8ef69cf02af66916ca8bb392a416e5e29f8593da
Moritz <moritz.pruefling@gmail.com>
2018-12-16 16:52:19 +0100

    Backend: Added logout page & logout logic

---
+++ backend/pages/logout_page.php
@@ -0,0 +1,15 @@
+
+<div id="loginContainer">
+
+    <?php
+
+        // Delete session
+        session_destroy();
+
+    ?>
+
+    <h2>Logout</h2>
+
+    <p>Du wurdest erfolgreich ausgeloggt!</p>
+
+</div>
