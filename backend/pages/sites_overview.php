31a60cc1f54d3afe9c78f2dac68d3d5ebd7882b4
Moritz <moritz.pruefling@gmail.com>
2019-01-08 17:46:53 +0100

    Backend: Improved sites overview styling

--- backend/pages/sites_overview.php
+++ backend/pages/sites_overview.php
@@ -2,7 +2,7 @@
 <html>
     <!-- Head of main page -->
     <head>
-        <link rel="stylesheet" type="text/css" href="css/list_style.css">
+        <link rel="stylesheet" type="text/css" href="css/sites_overview.css">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -16,52 +16,56 @@
     </head>
     
     <body>
-        <h1>Seitenverwalten</h1>
+        <h1>Seiten verwalten</h1>
         <!-- Seitenauswahl -->
-        <ol class="sortable">
-        <?php
-            // Function to check the string is ends  
-            // with given substring or not 
-            function endsWith($string, $endString) 
-            { 
-                $len = strlen($endString); 
-                if ($len == 0) { 
-                    return true; 
+        <div id="siteList">
+            <ol class="sortable">
+                <?php
+                // Function to check the string is ends  
+                // with given substring or not 
+                function endsWith($string, $endString) 
+                { 
+                    $len = strlen($endString); 
+                    if ($len == 0) { 
+                        return true; 
+                    } 
+                    return (substr($string, -$len) === $endString); 
                 } 
-                return (substr($string, -$len) === $endString); 
-            } 
 
-            $sql = "SELECT * FROM site ORDER BY NavIndex";
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                $sites = array();
-                $isend = "";
-                while ($row = mysqli_fetch_assoc($result)) {
-                    array_push($sites, [$row["ID"], $row["Name"], $row["NavIndex"], $row["Visible"], $row["TemplateID"]]);
-                    if(endsWith($row["NavIndex"],".1")) 
-                        echo "<ol>"; 
-                    elseif ($row["NavIndex"] != "1")
-                        echo "</li>"; 
+                $sql = "SELECT * FROM site ORDER BY NavIndex";
+                $result = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($result) > 0) {
+                    $sites = array();
+                    $isend = "";
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        array_push($sites, [$row["ID"], $row["Name"], $row["NavIndex"], $row["Visible"], $row["TemplateID"]]);
+                        if(endsWith($row["NavIndex"],".1")) 
+                            echo "<ol class='subItemList'>"; 
+                        elseif ($row["NavIndex"] != "1")
+                            echo "</li>"; 
+                        if (strpos($isend, ".") && !strpos($row["NavIndex"], "."))
+                            echo "</ol></li>";
+                        echo "<li id=\"siteid_" . $row["ID"] . "\" class=\"visible_" . $row["Visible"] . " template_" . $row["TemplateID"] . "\"><div class='listItem'><a href=\"index.php?page=Editor&loadcontenid=" . $row["ID"] ."&lang=de\">" . $row["Name"] . "</a><a class='editsite'>Bearbeiten</a></div>\n";
+                        $isend = $row["NavIndex"];
+                    }
                     if (strpos($isend, ".") && !strpos($row["NavIndex"], "."))
-                        echo "</ol></li>";
-                    echo "<li id=\"siteid_" . $row["ID"] . "\" class=\"visible_" . $row["Visible"] . " template_" . $row["TemplateID"] . "\"><div><a href=\"index.php?page=Editor&loadcontenid=" . $row["ID"] ."&lang=de\">" . $row["Name"] . "</a><span class='editsite' style='float:right; cursor:pointer'  >Edit</span></div>\n";
-                    $isend = $row["NavIndex"];
+                        echo "</li></ol>";
+                    echo "</li>";
                 }
-                if (strpos($isend, ".") && !strpos($row["NavIndex"], "."))
-                    echo "</li></ol>";
-                echo "</li>";
-            }
-            ?>
-        </ol>
+                ?>
+            </ol>
+        </div>
 
         <br>
-        <button id="savebutton" class="buttonStyle">Save!</button>
-        <button id="createbutton" class="buttonStyle">Add new Site</button>
-        <div hidden id="dialog_new" title="Add new Site">
-            Enter Sitename:
-            <input class="sitename_new" type="text" />
+        <button id="savebutton" class="buttonStyle">Speichern</button>
+        <button id="createbutton" class="buttonStyle">Neue Seite hinzufügen</button>
+
+
+        <div hidden id="dialog_new" title="Neue Seite hinzufügen">
+            Seitenname:
+            <input class="inputStyle sitename_new" type="text" />
             <br>
-            Enter Template ID:
+            Template:
             <select class="templateid_new">
                 <?php
                     $sql = "SELECT * FROM template ORDER BY ID";
@@ -77,17 +81,17 @@
             </select> 
 
             <br>
-            Is Visible?:
+            In Navigation einbinden:
             <input class="isvisible_new" type="checkbox" />
             <br>
-            <button class="newsitesaver">Save me!</button>
+            <button class="buttonStyle newsitesaver">Speichern</button>
         </div>
 
-        <div hidden id="dialog_edit" title="Edit Site">
-            Enter Sitename:
-            <input class="sitename" type="text" />
+        <div hidden id="dialog_edit" title="Seite bearbeiten">
+            Seitenname:
+            <input class="inputStyle sitename" type="text" />
             <br>
-            Enter Template ID:
+            Template:
             <select class="templateid">
                 <?php
                     $sql = "SELECT * FROM template ORDER BY ID";
@@ -102,11 +106,11 @@
                 ?>
             </select> 
             <br>
-            Is Visible?:
+            In Navigation einbinden:
             <input class="isvisible" type="checkbox" />
             <br>
-            <button class="editsitesaver">Save me!</button>
-            <button class="editsitedelete">Delete me!</button>
+            <button class="buttonStyle editsitesaver">Seite erstellen</button>
+            <button class="buttonStyle editsitedelete">Abbrechen</button>
             <p class="siteid" hidden></p>
         </div>
     </body>
