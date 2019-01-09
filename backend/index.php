78f07c1fb81a2ef1bcfc24536093829b9b3afc3d
Domi <d.flierl@oth-aw.de>
2019-01-09 12:31:11 +0100

    backend: added jquery ui

--- backend/index.php
+++ backend/index.php
@@ -15,7 +15,11 @@
         <link rel="stylesheet" type="text/css" media="screen" href="css/login_page.css" />
         <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
         <link rel="shortcut icon" type="image/x-icon" href="ico.png"/>
-        <script src="js/jquery-3.3.1.min.js"></script>
+        <!--script src="js/jquery-3.3.1.min.js"></script-->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <link rel="stylesheet" href="/resources/demos/style.css">
     </head>
     <body>
         <div class="wrapper">   
--- backend/pages/sites_overview.php
+++ backend/pages/sites_overview.php
@@ -3,9 +3,9 @@
     <!-- Head of main page -->
     <head>
         <link rel="stylesheet" type="text/css" href="css/sites_overview.css">
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <!--link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/nestedSortable/2.0.0/jquery.mjs.nestedSortable.min.js"></script>
         <script src="js/site_overview_script.js"></script>
         <style>
