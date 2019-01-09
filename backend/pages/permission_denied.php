e908450614dda0864d1c76c671bb04a7cd421bcf
Moritz <moritz.pruefling@gmail.com>
2019-01-09 11:27:06 +0100

    Backend: Redesigned permission denied page

--- backend/pages/permission_denied.php
+++ backend/pages/permission_denied.php
@@ -1,1 +1,8 @@
-<h1>Error 503: Permission denied</h1>
+<div id="loginContainer">
+
+    <h2>Kein Zugriff</h2>
+
+    <p>Du hast nicht genug Rechte, um diese Seite anzuzeigen.</p>
+    <p>Wenn du der Meinung bist, dass das ein Fehler ist, wende dich bitte an den Seitenadministrator.</p>
+
+</div>
